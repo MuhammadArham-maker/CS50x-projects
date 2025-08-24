da6dbb9be39dcb7293ffb027b0e56ac285bf0ef8
MuhammadArham-maker <mp1932140@gmail.com>
2025-08-24 09:36:49 +0530

    Add week6 mario_less.py

---
+++ Week6/mario_less.py
@@ -0,0 +1,15 @@
+from cs50 import get_int
+
+while True:
+    h = get_int("Height: ")
+    if 1 <= h <= 8:
+        break
+
+for row in range(h):
+
+    for space in range(h-(row+1)):
+        print(" ", end='')
+
+    for bricks in range(row+1):
+        print("#", end='')
+    print()
