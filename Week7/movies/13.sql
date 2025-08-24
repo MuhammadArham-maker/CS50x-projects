ad29d5c3fe28671293931a6366ddea7473b83e9c
MuhammadArham-maker <mp1932140@gmail.com>
2025-08-24 10:36:20 +0530

    Add 13.sql

---
+++ Week7/movies/13.sql
@@ -0,0 +1,11 @@
+-- Lists the names of all people who starred in a movie in which Kevin Bacon also starred
+SELECT DISTINCT people.name
+FROM people
+JOIN stars ON people.id = stars.person_id
+WHERE stars.movie_id IN (
+    SELECT stars.movie_id
+    FROM stars
+    JOIN people ON stars.person_id = people.id
+    WHERE people.name = 'Kevin Bacon' AND people.birth = 1958
+)
+AND NOT (people.name = 'Kevin Bacon' AND people.birth = 1958);
